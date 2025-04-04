859aa4358fbef956624ac49749d80c90d4a53d31
EthanZiv <ethanzivagbayani@gmail.com>
2025-04-05 06:09:25 +0800

    index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 document.getElementById('toggleButton').addEventListener('click', function() {
     var dropdown = document.getElementById('dropdown');
-    // Use getComputedStyle to ensure correct style handling
     if (dropdown.style.display === '' || dropdown.style.display === 'none') {
         dropdown.style.display = 'block';
     } else {
